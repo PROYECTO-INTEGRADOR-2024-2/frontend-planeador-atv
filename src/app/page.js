af7fb4987e45b9bdc6fe2fdd2b4141cedeecce25
Jonathan Granda <jhonatangranda.2002@gmail.com>
2024-09-30 23:47:33 -0500

    Implementación completa del footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer"
 
 export default function Home() {
   return (
     <div>
       <main></main>
       <Navbar />
+      <div>
+        pagina principal
+      </div>
+
+      <Footer></Footer>
     </div>
   );
 }
