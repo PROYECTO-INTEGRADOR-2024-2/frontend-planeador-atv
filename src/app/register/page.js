e9881f0cc8bf95c07921bc446ee1b10bbc23436b
“cristianmunoz1” <cristian.munoz3@udea.edu.co>
2024-10-12 11:17:38 -0500

    Se arregla el div de la imagen del formulario register

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,20 +1,22 @@
 import RegisterPersonForm from "@/components/RegisterPersonForm";
-import Image from 'next/image';
+import Image from "next/image";
 
-const register = () => {
+const Register = () => {
   return (
-    <div className="flex flex-row ">
-      <div className="basis-0  bg-no-repeat md:basis-3/5 bg-cover bg-slate-500 h-screen ">
+    <div className="flex flex-row">
+      <div className="relative basis-0 md:basis-3/5 bg-no-repeat bg-slate-500 h-screen">
         <Image
           src="/images/bg-login.png"
           alt="Background login image"
-          className="object-full shadow-blue-500/50 h-screen"
+          className="object-cover shadow-blue-500/50"
           layout="fill"
         />
       </div>
-      <RegisterPersonForm />
+      <div className="flex-grow">
+        <RegisterPersonForm />
+      </div>
     </div>
   );
 };
 
-export default register;
+export default Register;
