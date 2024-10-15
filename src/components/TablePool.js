8bb2eb8e9f67f1d25ed46eb080abc032c3fa595c
“cristianmunoz1” <cristian.munoz3@udea.edu.co>
2024-10-15 08:57:12 -0500

    Se agrega el pool de tutorías

---
+++ src/components/TablePool.js
@@ -0,0 +1,95 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const TablePool = ({ title, columns }) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [subject, setSubject] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:8080/api/v1/session/pool"
+        );
+        if (!response.ok) {
+          throw new Error("Respuesta no válida");
+        }
+        const result = await response.json();
+        setData(result.map((item) => Object.values(item)));
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchDataSubject = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/api/v1/subject/");
+        if (!response.ok) {
+          throw new Error("Respuesta no válida");
+        }
+        const result = await response.json();
+        setSubject(result.map((item) => Object.values(item)));
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+    fetchDataSubject();
+  }, []);
+
+  return (
+    <div className="bg-gray-100 rounded-lg shadow-md py-2 ">
+      <div className="bg-gray-200 mx-auto border border-slate-400 ">
+        <h1 className="text-3xl font-bold py-5 text-gray-600 mx-4">{title}</h1>
+      </div>
+      <div className="p-8">
+        <table className="min-w-full divide-y divide-gray-200 border-solid border-slate-400">
+          <thead className="bg-gray-50 border border-gray-400 bg-[#E5E7EB]">
+            <tr className="border border-slate-500">
+              {columns.map((item, rowIndex) => (
+                <th
+                  key={rowIndex}
+                  className="px-6 py-4 whitespace-nowrap border border-slate-300"
+                >
+                  {item?.toString() || ""}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="border border-slate-500">
+            {data.map((item) => (
+              <tr key={item[0]}>
+                <td className="px-6 py-4 whitespace-nowrap">{item[1]}</td>
+                <td className="px-6 py-4 whitespace-nowrap">{item[2]}</td>
+
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {subject.find((element) => element[0] === item[4])?.[2] ||
+                    "Materia no encontrada"}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">{item[5]}</td>
+                {/*Hora*/}
+                <td className="px-6 py-4 whitespace-nowrap">{item[6]}</td>
+                <td className="px-6 py-4 whitespace-nowrap flex flex-center">
+                  <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={() => {
+                      setId(item[0]);
+                      handleModalEditar(item[0]);
+                    }}
+                  >
+                    Aceptar
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default TablePool;
