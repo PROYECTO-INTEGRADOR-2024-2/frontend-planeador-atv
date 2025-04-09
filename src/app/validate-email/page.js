2b98a42dd5f215c076eb425d1fb909e144d75674
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2025-04-08 21:11:08 -0500

    Se agrega el login con Google

--- src/app/validate-email/page.js
+++ src/app/validate-email/page.js
@@ -2,10 +2,12 @@
 
 import { useEffect } from "react"
 import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation";
 
+import { jwtDecode } from "jwt-decode";
 export default function ValidateEmail() {
   const { data: session, status } = useSession()
-
+  const router = useRouter();
   useEffect(() => {
     if (status === "authenticated") {
       const email = session.user.email
@@ -18,10 +20,30 @@ export default function ValidateEmail() {
       })
         .then(res => res.json())
         .then(data => {
-          if (data) {
+          if (data === true) {
             console.log("El correo ya está registrado")
+            fetch("http://localhost:8081/api/v1/auth/google", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+              },
+              body: JSON.stringify({ email })
+            })
+              .then(res => res.json())
+              .then(authData => {
+                const user = jwtDecode(authData.token);
+                console.log("Token recibido:", authData.token)
+                localStorage.setItem("token", authData.token)
+                localStorage.setItem("user", JSON.stringify(user));
+                router.push("../landing")
+              })
+              .catch(err => console.error("Error al solicitar token:", err))
+
           } else {
             console.log("El correo es nuevo")
+            // Redirigir a completar registro, si quieres:
+            // window.location.href = "/complete-reg"
           }
         })
         .catch(err => console.error("Error al consultar backend:", err))
