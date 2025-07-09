a24c84de0c7dfde5cc4bc7704aaa7b020cbf9f08
Jonathan Granda <jhonatangranda.2002@gmail.com>
2025-07-08 21:03:23 -0500

    correciones

--- src/components/HeroN.js
+++ src/components/HeroN.js
@@ -6,9 +6,11 @@ import Image from "next/image";
 import fotoTest from "../../public/images/foto-test1.png";
 import fotoTest2 from "../../public/images/foto-test2.png";
 import fotoTest3 from "../../public/images/foto-test3.png";
+import { useRouter } from "next/navigation";
 
 const HeroN = () => {
   const [openFaq, setOpenFaq] = useState(null);
+  const router = useRouter();
 
   const toggleFaq = (index) => {
     if (openFaq === index) {
@@ -36,10 +38,18 @@ const HeroN = () => {
                 </p>
               </div>
               <div className="space-x-4 py-4">
-                <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-xl px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-xl px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                onClick={() => router.push("../register")}>
                   Empezar
                 </button>
-                <button variant="outline">Saber más</button>
+                <button variant="outline">
+                   <a
+                    href="https://fundacionantivirusparaladesercion.org/fundacion"
+                    target="_blank"
+                    className="hover:underline hover:text-black transition-colors duration-500"
+                  >
+                    Saber más
+                  </a></button>
               </div>
             </div>
           </div>
